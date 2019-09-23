b642d2bb8f8c5bc3a79cac1e4ff29fe979a3e56c
Titir Adhikary <titir.adhikary@hotmail.com>
2019-09-23 21:43:06 +0530

    Added post services

---
+++ lib/services.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'post.dart';
+import 'dart:io';
+
+String url = 'https://jsonplaceholder.typicode.com/posts';
+
+Future<Post> getPost() async{
+  final response = await http.get('$url/1');
+  return postFromJson(response.body);
+}
