2823436f04bd036179abd3a749b35ac102cfc48f
Titir Adhikary <titir.adhikary@hotmail.com>
2019-09-30 23:07:04 +0530

    Added some more fiiles

---
+++ lib/Text.dart
@@ -0,0 +1,12 @@
+/*Container(
+                              width: 300.0,
+                              child: new TextField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'City Name',
+                                ),
+                                cursorColor: Colors.cyan,
+                                controller: mycontroller,
+                              )
+                              ),
+                              */
